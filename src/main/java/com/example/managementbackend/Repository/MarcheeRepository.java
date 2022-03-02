d0fe55e347997028e96bb9261eb1946df27da594
Bochra Nahali <bochra.nahalii@gmail.com>
2022-03-02 01:45:09 +0100

    add find by code to marchee

--- src/main/java/com/example/managementbackend/Repository/MarcheeRepository.java
+++ src/main/java/com/example/managementbackend/Repository/MarcheeRepository.java
@@ -2,6 +2,7 @@ package com.example.managementbackend.Repository;
 
 import com.example.managementbackend.model.Entreprise;
 import com.example.managementbackend.model.Marchee;
+import com.example.managementbackend.model.Secteur;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,5 +15,6 @@ import java.util.Optional;
 public interface MarcheeRepository extends JpaRepository<Marchee, Long> {
     List<Marchee> findByOrgId(long organisationId);
     Optional<Marchee> findByIdAndOrgId(long id, long organisationId);
+    Optional<Marchee> findByCode(String code);
 
 }
