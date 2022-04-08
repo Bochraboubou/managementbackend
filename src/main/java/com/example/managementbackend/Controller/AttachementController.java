10d9f1597a94bcc56dedb4c8aeccecbb9ccaa1b3
Bochra Nahali <bochra.nahalii@gmail.com>
2022-04-08 16:16:47 +0100

    add method add attachement

--- src/main/java/com/example/managementbackend/Controller/AttachementController.java
+++ src/main/java/com/example/managementbackend/Controller/AttachementController.java
@@ -2,13 +2,23 @@ package com.example.managementbackend.Controller;
 
 import com.example.managementbackend.Service.AttachemntService;
 import com.example.managementbackend.Service.BondeCommandeService;
+import com.example.managementbackend.model.Attachement;
+import com.example.managementbackend.model.Metier;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequestMapping("/admin")
 @RestController
 public class AttachementController {
     @Autowired
     private AttachemntService attachementService;
+
+    @PostMapping("/bondecommande/{bondeCommandeId}/attachement")
+    public Attachement createAttachement(@PathVariable(value = "bondeCommandeId") Long bondeCommandeId,
+                                         @Valid @RequestBody Attachement attachement) {
+        return attachementService.create(bondeCommandeId,attachement);
+    }
+
 }
--- src/main/java/com/example/managementbackend/Service/AttachemntService.java
+++ src/main/java/com/example/managementbackend/Service/AttachemntService.java
@@ -2,6 +2,9 @@ package com.example.managementbackend.Service;
 
 import com.example.managementbackend.Repository.AttachementRepository;
 import com.example.managementbackend.Repository.BondeCommandeRepository;
+import com.example.managementbackend.exception.ResourceNotFoundException;
+import com.example.managementbackend.model.Attachement;
+import com.example.managementbackend.model.Metier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,14 @@ import org.springframework.stereotype.Service;
 public class AttachemntService {
     @Autowired
     private AttachementRepository attachementRepo;
+
+    @Autowired
+    private BondeCommandeRepository bondeCommandeRepo;
+
+    public Attachement create(Long bondeCommandeId, Attachement attachement) {
+        return bondeCommandeRepo.findById(bondeCommandeId).map(bondeCommande -> {
+            attachement.setBonDeCommande(bondeCommande);
+            return attachementRepo.save(attachement);
+        }).orElseThrow(() -> new ResourceNotFoundException("bondeCommandeId " + bondeCommandeId+ " not found"));
+    }
 }
